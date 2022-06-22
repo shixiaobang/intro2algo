409927eb1ac191f6514203445d7ad7179da8261c
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-06-22 17:04:00 +0800

    Create binary_heap.h

---
+++ II Sorting and Statistics/binary_heap/binary_heap.h
@@ -0,0 +1,27 @@
+/*
+    Binary Heap
+    Author: shixiaobang
+    Mail: xiaobang.shi@outlook.com
+*/
+#if !defined(BINARY_HEAP_H)
+#define BINARY_HEAP_H
+
+// Parent of A[i]
+int parent(int i);
+
+// Left child of A[i]
+int left(int i);
+
+// Right child of A[i]
+int right(int i);
+
+// Maintain the max-heap property of A[i]
+int max_heapify(int *array, int heap_size, int i);
+
+// Produce a max-heap from an unordered input array A[0 : n - 1]
+int build_max_heap(int *array, int n);
+
+// Heapsort array A[0 : n - 1]
+int heapsort(int *array, int n);
+
+#endif // BINARY_HEAP_H
