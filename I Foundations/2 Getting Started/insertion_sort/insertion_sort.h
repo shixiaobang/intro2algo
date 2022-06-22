59bb1f32d6e4cd9b698a845630d01c8afa998525
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-06-22 17:01:14 +0800

    Create insertion_sort.h

---
+++ I Foundations/2 Getting Started/insertion_sort/insertion_sort.h
@@ -0,0 +1,12 @@
+/*
+    Insertion Sort
+    Author: shixiaobang
+    Mail: xiaobang.shi@outlook.com
+*/
+#if !defined(INSERTION_SORT_H)
+#define INSERTION_SORT_H
+
+// Insertion Sort A[0 : n - 1]
+int insertion_sort(int *array, int n);
+
+#endif // INSERTION_SORT_H
