980be98bffb1d8decf864f7853c66fbfa1b4baf9
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-06-22 17:02:33 +0800

    Create merge_sort.h

---
+++ I Foundations/2 Getting Started/merge_sort/merge_sort.h
@@ -0,0 +1,15 @@
+/*
+    Merge Sort
+    Author: shixiaobang
+    Mail: xiaobang.shi@outlook.com
+*/
+#if !defined(MERGE_SORT_H)
+#define MERGE_SORT_H
+
+// Merge the two sorted subarrays A[p : q] and A[q + 1 : r] back into A[p : r]
+int merge(int *array, int p, int q, int r);
+
+// Mergesort array A[p : r]
+int merge_sort(int *array, int p, int r);
+
+#endif // MERGE_SORT_H
