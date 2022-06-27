41e3fdf939343879e0dfd941ebab0866faa06bf6
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-06-27 22:17:56 +0800

    Create stack.c

---
+++ III Data Structures/10 Elementary Data Structures/stack/stack.c
@@ -0,0 +1,75 @@
+#include "stack.h"
+#include <stdlib.h>
+
+stack *stack_ctor(int size)
+{
+    stack *s = (stack *)malloc(sizeof(stack));
+    s->p_key = (int *)malloc(sizeof(int) * size);
+
+    s->top = -1;
+    s->size = size;
+
+    return s;
+}
+
+int stack_dtor(stack *s)
+{
+    free(s->p_key);
+    free(s);
+
+    return 0;
+}
+
+bool stack_empty(const stack *s)
+{
+    if (s->top == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool stack_full(const stack *s)
+{
+    if (s->top == s->size - 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int push(stack *s, int key)
+{
+    int full = stack_full(s);
+    if (full)
+    {
+        return 1;
+    }
+    else
+    {
+        s->top += 1;
+        *(s->p_key + s->top) = key;
+
+        return 0;
+    }
+}
+
+int pop(stack *s)
+{
+    int flag = stack_empty(s);
+    if (flag)
+    {
+        return -1;
+    }
+    else
+    {
+        s->top -= 1;
+        return *(s->p_key + s->top + 1);
+    }
+}
