9799227a4b53a5524f666f8d106c356c9543f332
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-07-01 21:55:31 +0800

    Update singly_linked_list.h

--- III Data Structures/10 Elementary Data Structures/singly_linked_list/singly_linked_list.h
+++ III Data Structures/10 Elementary Data Structures/singly_linked_list/singly_linked_list.h
@@ -1,14 +1,22 @@
 #if !defined(SINGLY_LINKED_LIST_H)
 #define SINGLY_LINKED_LIST_H
 
-typedef struct singly_linked_list
+typedef struct singly_linked_list_element
 {
     int key;
-    struct singly_linked_list *next;
-} sll;
+    struct singly_linked_list_element *next;
+} sll_element;
 
-sll *singly_linked_list_ctor();
+typedef sll_element *sll;
 
-int singly_linked_list_dtor(sll *list);
+sll *sll_ctor();
+
+int sll_search(sll *head, int key);
+
+int sll_prepend(sll *head, int key);
+
+int sll_insert(sll *head, int index, int key);
+
+int sll_delete(sll *head, int index);
 
 #endif // SINGLY_LINKED_LIST_H
