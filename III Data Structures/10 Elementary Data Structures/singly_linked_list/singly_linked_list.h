d3ceca669c2ec5a753bbfba54c7420aabced3dca
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-07-02 22:59:23 +0800

    Update singly_linked_list.h

--- III Data Structures/10 Elementary Data Structures/singly_linked_list/singly_linked_list.h
+++ III Data Structures/10 Elementary Data Structures/singly_linked_list/singly_linked_list.h
@@ -9,14 +9,29 @@ typedef struct singly_linked_list_element
 
 typedef sll_element *sll;
 
+// Initialize a singly linked list
 sll *sll_ctor();
 
+// Destroy a singly linked list
+int sll_dtor(sll *head);
+
+// Return the length of a singly linked list
+int sll_length(sll *head);
+
+// Return index of an element with key value in singly linked list
 int sll_search(sll *head, int key);
 
 int sll_prepend(sll *head, int key);
 
+int sll_append(sll *head, int key);
+
+// Insert a new element after an element at a specific index in a singly linked list
 int sll_insert(sll *head, int index, int key);
 
+// Delete element of specific index in singly linked list
 int sll_delete(sll *head, int index);
+
+// Return key value from specific index of singly linked list
+int sll_key(sll *head, int index);
 
 #endif // SINGLY_LINKED_LIST_H
