d1fedb2c761896c16de6636dda72d1f1f1c4afa5
Xiaobang Shi <xiaobang.shi@outlook.com>
2022-07-03 11:23:23 +0800

    Update doubly_linked_list.h

--- III Data Structures/10 Elementary Data Structures/doubly_linked_list/doubly_linked_list.h
+++ III Data Structures/10 Elementary Data Structures/doubly_linked_list/doubly_linked_list.h
@@ -1,15 +1,40 @@
 #if !defined(DOUBLY_LINKED_LIST_H)
 #define DOUBLY_LINKED_LIST_H
 
-typedef struct doubly_linked_list
+typedef struct doubly_linked_list_element
 {
     int key;
-    struct doubly_linked_list *prev;
-    struct doubly_linked_list *next;
-} dll;
+    struct doubly_linked_list_element *prev;
+    struct doubly_linked_list_element *next;
+} dll_element;
 
-dll *doubly_linked_list_ctor();
+typedef dll_element *dll;
 
-int doubly_linked_list_dtor(dll *list);
+// Initialize a doubly linked list
+dll *dll_ctor();
+
+// Destroy a doubly linked list
+int dll_dtor(dll *head);
+
+// Return the length of a doubly linked list
+int dll_length(dll *head);
+
+// Return the index of an element with a specific key value in a doubly linked list
+int dll_search(dll *head, int key);
+
+// Insert a new element at the head of a doubly linked list
+int dll_prepend(dll *head, int key);
+
+// Insert a new element at the tail of a doubly linked list
+int dll_append(dll *head, int key);
+
+// Insert a new element after an element at a specific index in a doubly linked list
+int dll_insert(dll *head, int index, int key);
+
+// Delete an element at a specific index in a doubly linked list
+int dll_delete(dll *head, int index);
+
+// Return key value from specific index of doubly linked list
+int dll_key(dll *head, int index);
 
 #endif // DOUBLY_LINKED_LIST_H
