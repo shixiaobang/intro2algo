d67ae332df994980a5c476ae74a8ac5726ff2f53
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-07 22:44:36 +0800

    Update adjacency_list.h

--- VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.h
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.h
@@ -1,78 +1,31 @@
 #if !defined(ADJACENCY_LIST_H)
 #define ADJACENCY_LIST_H
 
-#include "../edge/edge.h"
-
-typedef struct vertex // vertex
+typedef struct al_element // adjacency list element (vertex)
 {
-    int index;           // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
-    struct vertex *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
-} vertex;
+    int index;               // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
+    struct al_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
+} al_element;
 
-typedef struct vertex_edge_weight // vertex with edge weight
-{
-    int index;
-    int weight; // weight of an edge formed by a vertex and its adjacent vertex (undirected graph) or initial vertex and its terminal vertex (directed graph)
-    struct vertex_edge_weight *next;
-} vertex_edge_weight;
-
-typedef vertex **adj_list;
-
-typedef vertex_edge_weight **weighted_adj_list;
-
-typedef struct
-{
-    adj_list list; // adjacent list of graph
-    int v;         // number of vertices
-    int e;         // number of edges
-} graph_adj_list;
-
-typedef struct
-{
-    weighted_adj_list list; // adjacent list of weighted graph
-    int v;                  // number of vertices
-    int e;                  // number of edges
-} weighted_graph_adj_list;
+typedef al_element *al;
 
 /**
- * Create an adjacency-list representation of graph
- * @param v number of vertices
- * @return pointer to graph
+ * Initialize an adjacency list array
+ * @return pointer to the head of the first adjacency list
  */
-graph_adj_list *graph_adj_list_ctor(int v);
+al *al_array_ctor(const int v);
 
 /**
- * Destroy an adjacency-list representation of graph
- * @param graph adjacent list representation of a graph
+ * Destroy an adjacency list array
+ * @param head double pointer to the head of the first adjacency list
  */
-int graph_adj_list_dtor(graph_adj_list *graph);
+int al_array_dtor(al **head, const int v);
 
 /**
- * Add a edge to a adjacent list representation graph
- * @param graph adjacent list representation of a graph
- * @param edge undirected edge
+ * Prepend vertex to adjacency list
+ * @param head pointer to the head of adjacency list
+ * @param index index of vertex
  */
-int add_undirected_edge(graph_adj_list *graph, const edge *edge);
-
-/**
- * Add a directed edge to a adjacent list representation graph
- * @param graph adjacent list representation of a graph
- * @param edge directed edge
- */
-int add_directed_edge(graph_adj_list *graph, const edge *edge);
-
-/**
- * Add a weighted undirected edge to a adjacent list representation graph
- * @param graph adjacent list representation of a graph
- * @param weight_edge weighted undirected edge
- */
-int add_weighted_undirected_edge(graph_adj_list *graph, const weighted_edge *weight_edge);
-
-/**
- * Add a weighted directed edge to a adjacent list representation graph
- * @param graph adjacent list representation of a graph
- * @param weight_edge weighted directed edge
- */
-int add_weighted_directed_edge(graph_adj_list *graph, const weighted_edge *weight_edge);
+int al_prepend(al *head, const int index);
 
 #endif // ADJACENCY_LIST_H
