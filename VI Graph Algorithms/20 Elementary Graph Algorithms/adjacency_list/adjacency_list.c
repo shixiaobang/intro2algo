74e859b3030d501c56699ecee870371c0b0c1662
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-07 22:44:49 +0800

    Update adjacency_list.c

--- VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.c
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.c
@@ -2,127 +2,93 @@
 #include <stdlib.h>
 
 /**
+ * Initialize a vertex
  * @param index index of vertex
  * @return pointer to vertex
  */
-vertex *vertex_ctor(int index);
+static al_element *al_element_ctor(const int index);
 
 /**
- * @param head double pointer to head of adjacency list
- * @param index index of vertex
+ * Destroy a adjacency list
+ * @param head
  */
-int vertex_prepend(vertex **head, int index);
+static int al_dtor(al *head);
 
-graph_adj_list *graph_adj_list_ctor(int v)
+al *al_array_ctor(const int v)
 {
-    graph_adj_list *graph = (graph_adj_list *)malloc(sizeof(graph_adj_list));
-    if (graph != NULL)
+    al *head = (al *)malloc(sizeof(al_element *) * v);
+    if (head != NULL)
     {
-        graph->v = v;
-        graph->e = 0;
-
-        graph->list = (vertex **)malloc(sizeof(vertex *) * v);
-        if (graph->list != NULL)
+        for (int i = 0; i < v; i++)
         {
-            for (int i = 0; i < v; i++)
-            {
-                *(graph->list + i) = NULL;
-            }
-
-            return graph;
-        }
-        else
-        {
-            return NULL;
+            *(head + i) = NULL;
         }
     }
     else
     {
-        return NULL;
+        /* do nothing */
     }
+
+    return head;
 }
 
-int graph_adj_list_dtor(graph_adj_list *graph)
+int al_array_dtor(al **head, const int v)
 {
-    vertex **head;   // head of vertices
-    const vertex *p; // pointer to vertex
-    for (int i = 0; i < graph->v; i++)
+    for (int i = 0; i < v; i++)
     {
-        head = (graph->list + i);
-        if (*head != NULL)
-        {
-            p = (*head)->next;
-            while (p != NULL)
-            {
-                free(*head);
-                *head = NULL;
-
-                *head = (vertex *)p;
-                p = (*head)->next;
-            }
-            free(*head);
-            *head = NULL;
-        }
-        else
-        {
-            /* do nothing */
-        }
+        al_dtor(*head + i);
     }
-
-    free(graph->list);
-    graph->list = NULL;
-
-    free(graph);
-    /* graph = NULL; */
+    free(*head);
+    *head = NULL;
 
     return 0;
 }
 
-int add_undirected_edge(graph_adj_list *graph, const edge *edge)
+int al_prepend(al *head, const int index)
 {
-    int u = edge->u;
-    int v = edge->v;
-
-    vertex_prepend(graph->list + u, v); // add u to list[v]
-    vertex_prepend(graph->list + v, u); // add v to list[u]
-
-    graph->e += 1;
-
-    return 0;
-}
-
-int add_directed_edge(graph_adj_list *graph, const edge *edge)
-{
-}
-
-int add_weighted_undirected_edge(graph_adj_list *graph, const weighted_edge *weight_edge)
-{
-}
-
-int add_weighted_directed_edge(graph_adj_list *graph, const weighted_edge *weight_edge)
-{
-}
-
-vertex *vertex_ctor(int index)
-{
-    vertex *v = (vertex *)malloc(sizeof(vertex));
-    v->index = index;
-    v->next = NULL;
-
-    return v;
-}
-
-int vertex_prepend(vertex **head, int index)
-{
-    vertex *v = vertex_ctor(index);
+    al_element *element = al_element_ctor(index);
     if (*head != NULL)
     {
-        *head = v;
+        element->next = *head;
+        *head = element;
     }
     else
     {
-        v->next = *head;
-        *head = v;
+        *head = element;
     }
+
     return 0;
+}
+
+static al_element *al_element_ctor(const int index)
+{
+    al_element *element = (al_element *)malloc(sizeof(al_element));
+    element->index = index;
+    element->next = NULL;
+
+    return element;
+}
+
+static int al_dtor(al *head)
+{
+    if (*head != NULL)
+    {
+        const al_element *p = (*head)->next;
+        while (p != NULL)
+        {
+            free(*head);
+            *head = NULL;
+
+            *head = (al_element *)p;
+            p = (*head)->next;
+        }
+        free(*head);
+        *head = NULL;
+
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
