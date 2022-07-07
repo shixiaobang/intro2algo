8101355500d036743ada3bdea445db05b9f98a6a
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-07 23:49:42 +0800

    Create weighted_adjacency_list.h

---
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/weighted_adjacency_list.h
@@ -0,0 +1,33 @@
+#if !defined(WEIGHTED_ADJACENCY_LIST_H)
+#define WEIGHTED_ADJACENCY_LIST_H
+
+typedef struct wal_element // weighted adjacency list element (vertex with edge weight)
+{
+    int index;                // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
+    int weight;               // weight of edge formed by vertex and its adjacent vertex (undirected graph) or initial vertex and its terminal vertex (directed graph)
+    struct wal_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
+} wal_element;
+
+typedef wal_element *wal;
+
+/**
+ * Initialize a weighted adjacency list array
+ * @return pointer to the head of the first weighted adjacency list
+ */
+wal *wal_array_ctor(const int v);
+
+/**
+ * Destroy a weighted adjacency list array
+ * @param head double pointer to the head of the first weighted adjacency list
+ */
+int wal_array_dtor(wal **head, const int v);
+
+/**
+ * Prepend vertex with edge weight to weighted adjacency list
+ * @param head pointer to the head of weighted adjacency list
+ * @param index index of vertex
+ * @param weight weight of edge
+ */
+int wal_prepend(wal *head, const int index, const int weight);
+
+#endif // WEIGHTED_ADJACENCY_LIST_H
