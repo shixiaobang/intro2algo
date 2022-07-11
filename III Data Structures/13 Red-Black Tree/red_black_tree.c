4cc648f2bfe5e6881634e5a5673c0403cd13d394
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-11 17:30:34 +0800

    Create red_black_tree.c

---
+++ III Data Structures/13 Red-Black Tree/red_black_tree.c
@@ -0,0 +1,20 @@
+#include "red_black_tree.h"
+
+/**
+ * Initialize a red-black tree node
+ * @param
+ * @return pointer to red-black tree node
+ */
+static rbt_node *rbt_node_ctor(/* param */);
+
+rbt *rbt_ctor()
+{
+}
+
+int rbt_dtor(rbt **root)
+{
+}
+
+static rbt_node *rbt_node_ctor(/* param */)
+{
+}
