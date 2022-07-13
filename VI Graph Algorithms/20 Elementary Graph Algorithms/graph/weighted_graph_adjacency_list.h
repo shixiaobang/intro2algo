4d0f3a7350458a9ee5850e6ecc5ed7b3cdbfd1cb
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 21:13:43 +0800

    Update weighted_graph_adjacency_list.h

--- VI Graph Algorithms/20 Elementary Graph Algorithms/graph/weighted_graph_adjacency_list.h
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/graph/weighted_graph_adjacency_list.h
@@ -5,23 +5,23 @@
 
 typedef struct
 {
-    wal *head; // adjacent list array of weighted graph
+    walist *head; // adjacent list array of weighted graph
     int v;     // number of vertices
     int e;     // number of edges
-} graph_wal;
+} graph_walist;
 
 /**
  * Create a weighted graph represented by an adjacency list
  * @param v number of vertices
  * @return pointer to weighted graph
  */
-graph_wal *graph_wal_ctor(const int v);
+graph_walist *graph_walist_ctor(const int v);
 
 /**
  * Destroy a weighted graph represented by an adjacency list
  * @param graph double pointer to weighted graph
  */
-int graph_wal_dtor(graph_wal **graph);
+int graph_walist_dtor(graph_walist **graph);
 
 /**
  * Add a weighted undirected edge to a weighted graph represented by an adjacency list
@@ -29,7 +29,7 @@ int graph_wal_dtor(graph_wal **graph);
  * @param u index of edge's one endpoint
  * @param v index of edge's another endpoint
  */
-int graph_wal_add_ue(graph_wal *graph, const int u, const int v, const int weight);
+int graph_walist_add_ue(graph_walist *graph, const int u, const int v, const int weight);
 
 /**
  * Add a weighted directed edge to a weighted graph represented by an adjacency list
@@ -37,6 +37,6 @@ int graph_wal_add_ue(graph_wal *graph, const int u, const int v, const int weigh
  * @param u index of directed edge's initial vertex
  * @param v index of directed edge's terminal vertex
  */
-int graph_wal_add_de(graph_wal *graph, const int u, const int v, const int weight);
+int graph_walist_add_de(graph_walist *graph, const int u, const int v, const int weight);
 
 #endif // WEIGHTED_GRAPH_ADJACENCY_LIST_H
