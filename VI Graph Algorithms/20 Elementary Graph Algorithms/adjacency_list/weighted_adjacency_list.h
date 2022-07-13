c228a59548e3ece66e8bec36bc27aabce916e8ed
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 21:00:56 +0800

    Update weighted_adjacency_list.h

--- VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/weighted_adjacency_list.h
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/weighted_adjacency_list.h
@@ -1,26 +1,26 @@
 #if !defined(WEIGHTED_ADJACENCY_LIST_H)
 #define WEIGHTED_ADJACENCY_LIST_H
 
-typedef struct wal_element // weighted graph's adjacency list element (vertex with edge weight)
+typedef struct walist_element // weighted graph's adjacency list element (vertex with edge weight)
 {
-    int index;                // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
-    int weight;               // weight of edge formed by vertex and its adjacent vertex (undirected graph) or initial vertex and its terminal vertex (directed graph)
-    struct wal_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
-} wal_element;
+    int index;                   // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
+    int weight;                  // weight of edge formed by vertex and its adjacent vertex (undirected graph) or initial vertex and its terminal vertex (directed graph)
+    struct walist_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
+} walist_element;
 
-typedef wal_element *wal;
+typedef walist_element *walist;
 
 /**
  * Initialize a weighted graph's adjacency list array
  * @return pointer to the head of the first weighted graph's adjacency list
  */
-wal *wal_array_ctor(const int v);
+walist *walist_array_ctor(const int v);
 
 /**
  * Destroy a weighted graph's adjacency list array
  * @param head double pointer to the head of the first adjacency list
  */
-int wal_array_dtor(wal **head, const int v);
+int walist_array_dtor(walist **head, const int v);
 
 /**
  * Prepend vertex with edge weight to weighted graph's adjacency list
@@ -28,6 +28,6 @@ int wal_array_dtor(wal **head, const int v);
  * @param index index of vertex
  * @param weight weight of edge
  */
-int wal_prepend(wal *head, const int index, const int weight);
+int walist_prepend(walist *head, const int index, const int weight);
 
 #endif // WEIGHTED_ADJACENCY_LIST_H
