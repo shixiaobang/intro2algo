46eeb741e6d29e16eb55b31a7436e690b5160943
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 16:41:28 +0800

    Update binary_tree.h

--- III Data Structures/10 Elementary Data Structures/binary_tree/binary_tree.h
+++ III Data Structures/10 Elementary Data Structures/binary_tree/binary_tree.h
@@ -7,38 +7,38 @@ typedef struct binary_tree_node
     struct binary_tree_node *parent;
     struct binary_tree_node *left_child;
     struct binary_tree_node *right_child;
-} bint_node;
+} bintree_node;
 
-typedef bint_node *bint;
+typedef bintree_node *bintree;
 
 /**
  * Initialize a binary tree
  * @return pointer to the root of binary tree
  */
-bint *bint_ctor();
+bintree *bintree_ctor();
 
 /**
  * Destroy a binary tree
  * @param root pointer to the root of binary tree
  */
-int bint_dtor(bint **root);
+int bintree_dtor(bintree **root);
 
 /**
  * Preorder traversal of binary tree
  * @param root pointer to the root of a binary tree or its subtrees
  */
-void bint_preorder_traversal(const bint *root);
+void bintree_preorder_traversal(const bintree *root);
 
 /**
  * Inorder traversal of binary tree
  * @param root pointer to the root of a binary tree or its subtrees
  */
-void bint_inorder_traversal(const bint *root);
+void bintree_inorder_traversal(const bintree *root);
 
 /**
  * Postorder traversal of binary tree
  * @param root pointer to the root of a binary tree or its subtrees
  */
-void bint_postorder_traversal(const bint *root);
+void bintree_postorder_traversal(const bintree *root);
 
 #endif // BINARY_TREE_H
