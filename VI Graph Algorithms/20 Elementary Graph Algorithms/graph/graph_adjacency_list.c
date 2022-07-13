71938c59c7dd61d52a29fdaee35e27215e3eb9a7
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 21:14:16 +0800

    Update graph_adjacency_list.c

--- VI Graph Algorithms/20 Elementary Graph Algorithms/graph/graph_adjacency_list.c
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/graph/graph_adjacency_list.c
@@ -1,12 +1,12 @@
 #include "graph_adjacency_list.h"
 #include <stdlib.h>
 
-graph_al *graph_al_ctor(const int v)
+graph_alist *graph_alist_ctor(const int v)
 {
-    graph_al *graph = (graph_al *)malloc(sizeof(graph_al));
+    graph_alist *graph = (graph_alist *)malloc(sizeof(graph_alist));
     if (graph != NULL)
     {
-        graph->head = al_array_ctor(v);
+        graph->head = alist_array_ctor(v);
         graph->v = v;
         graph->e = 0;
     }
@@ -14,9 +14,9 @@ graph_al *graph_al_ctor(const int v)
     return graph;
 }
 
-int graph_al_dtor(graph_al **graph)
+int graph_alist_dtor(graph_alist **graph)
 {
-    al_array_dtor(&((*graph)->head), (*graph)->v); // free adjacency list array
+    alist_array_dtor(&((*graph)->head), (*graph)->v); // free adjacency list array
 
     free(*graph);
     *graph = NULL;
@@ -24,19 +24,19 @@ int graph_al_dtor(graph_al **graph)
     return 0;
 }
 
-int graph_al_add_ue(graph_al *graph, const int u, const int v)
+int graph_alist_add_ue(graph_alist *graph, const int u, const int v)
 {
-    al_prepend(graph->head + v, u); // prepend u to head[v]
-    al_prepend(graph->head + u, v); // prepend v to head[u]
+    alist_prepend(graph->head + v, u); // prepend u to head[v]
+    alist_prepend(graph->head + u, v); // prepend v to head[u]
 
     graph->e += 1;
 
     return 0;
 }
 
-int graph_al_add_de(graph_al *graph, const int u, const int v)
+int graph_alist_add_de(graph_alist *graph, const int u, const int v)
 {
-    al_prepend(graph->head + u, v); // prepend u to head[v]
+    alist_prepend(graph->head + u, v); // prepend u to head[v]
 
     graph->e += 1;
 
