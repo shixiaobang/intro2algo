25b19afb98edf895dcf5eabc1c140501ab6924a6
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 21:01:01 +0800

    Update adjacency_list.h

--- VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.h
+++ VI Graph Algorithms/20 Elementary Graph Algorithms/adjacency_list/adjacency_list.h
@@ -1,31 +1,31 @@
 #if !defined(ADJACENCY_LIST_H)
 #define ADJACENCY_LIST_H
 
-typedef struct al_element // adjacency list element (vertex)
+typedef struct alist_element // adjacency list element (vertex)
 {
-    int index;               // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
-    struct al_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
-} al_element;
+    int index;                  // index of vertex's adjacent vertex (undirected graph) or initial vertex's terminal vertex (directed graph)
+    struct alist_element *next; // pointer to next adjacent vertex (undirected graph) or terminal vertex (directed graph)
+} alist_element;
 
-typedef al_element *al;
+typedef alist_element *alist;
 
 /**
  * Initialize an adjacency list array
  * @return pointer to the head of the first adjacency list
  */
-al *al_array_ctor(const int v);
+alist *alist_array_ctor(const int v);
 
 /**
  * Destroy an adjacency list array
  * @param head double pointer to the head of the first adjacency list
  */
-int al_array_dtor(al **head, const int v);
+int alist_array_dtor(alist **head, const int v);
 
 /**
  * Prepend vertex to adjacency list
  * @param head pointer to the head of adjacency list
  * @param index index of vertex
  */
-int al_prepend(al *head, const int index);
+int alist_prepend(alist *head, const int index);
 
 #endif // ADJACENCY_LIST_H
