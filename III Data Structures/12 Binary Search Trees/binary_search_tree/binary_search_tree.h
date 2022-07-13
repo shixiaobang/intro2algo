5b7d54cb7d779996d77eaf9af21890f12f8a197a
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 16:41:24 +0800

    Update binary_search_tree.h

--- III Data Structures/12 Binary Search Trees/binary_search_tree/binary_search_tree.h
+++ III Data Structures/12 Binary Search Trees/binary_search_tree/binary_search_tree.h
@@ -3,53 +3,53 @@
 
 #include "../../10 Elementary Data Structures/binary_tree/binary_tree.h"
 
-typedef bint_node binst_node;
+typedef bintree_node binstree_node;
 
-typedef binst_node *binst;
+typedef binstree_node *binstree;
 
 /**
  * Search for a node with a given key in a binary search tree
  * @param root pointer to the root of a binary search tree or its subtrees
  * @param key a given key value
  */
-binst_node *binst_search(const binst *root, int key);
+binstree_node *binstree_search(const binstree *root, int key);
 
 /**
  * Return an element in a binary search tree whose key is a minimum
  * @param root pointer to the root of a binary search tree or its subtrees
  */
-binst_node *binst_minimum(const binst *root);
+binstree_node *binstree_minimum(const binstree *root);
 
 /**
  * Return an element in a binary search tree whose key is a maximum
  * @param root pointer to the root of a binary search tree or its subtrees
  */
-binst_node *binst_maximum(const binst *root);
+binstree_node *binstree_maximum(const binstree *root);
 
 /**
  * Return the successor of a node as the next node visited in an inorder traversal
  * @param node pointer to a given node of binary search tree
  */
-binst_node *binst_successor(const binst_node *node);
+binstree_node *binstree_successor(const binstree_node *node);
 
 /**
  * Return the predecessor of a node as the previous node visited in an inorder traversal
  * @param node pointer to a given node of binary search tree
  */
-binst_node *binst_predecessor(const binst_node *node);
+binstree_node *binstree_predecessor(const binstree_node *node);
 
 /**
  * Insert a new node with given key value into a binary search tree
  * @param root pointer to the root of a binary search tree
  * @param key a given key value
  */
-int binst_insert(binst *root, int key);
+int binstree_insert(binstree *root, int key);
 
 /**
  * Delete a node in a binary search tree
  * @param root pointer to the root of a binary search tree
  * @param node double pointer to the node to delete
  */
-int binst_delete(binst *root, binst_node **node);
+int binstree_delete(binstree *root, binstree_node **node);
 
 #endif // BINARY_SEARCH_TREE_H
