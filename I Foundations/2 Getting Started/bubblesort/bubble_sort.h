1a9a59ff0ef435d3e894ff972041cf4eac8bcbfc
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-13 22:35:52 +0800

    Create bubble_sort.h

---
+++ I Foundations/2 Getting Started/bubblesort/bubble_sort.h
@@ -0,0 +1,11 @@
+#if !defined(BUBBLE_SORT_H)
+#define BUBBLE_SORT_H
+
+/**
+ * Bubble sort an array
+ * @param array pointer to array
+ * @param n length of array
+ */
+int bubble_sort(int *array, int n);
+
+#endif // BUBBLE_SORT_H
