7b85293c31ad4a45eb67219723805561e7475dec
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-14 20:29:14 +0800

    Update quicksort.h

--- II Sorting and Statistics/7 Quicksort/quicksort/quicksort.h
+++ II Sorting and Statistics/7 Quicksort/quicksort/quicksort.h
@@ -1,8 +1,20 @@
 #if !defined(QUICKSORT_H)
 #define QUICKSORT_H
 
+/**
+ * Quicksort the array A[p : r]
+ * @param array pointer to array
+ * @param p first index of array
+ * @param r last  index of array
+ */
 int quicksort(int *array, int p, int r);
 
+/**
+ * Partition the array A[p : r] into two (possibly empty) subarrays A[p : q – 1] (the low side) and A[q + 1 : r] (the high side)
+ * @param array pointer to array
+ * @param p first index of array
+ * @param r last  index of array
+ */
 int partition(int *array, int p, int r);
 
 #endif // QUICKSORT_H
