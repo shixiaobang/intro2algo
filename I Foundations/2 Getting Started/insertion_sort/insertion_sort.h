935005fab7746bddc3496904f183f61fe821f2cb
Xiaobang Shi <52265448+shixiaobang@users.noreply.github.com>
2022-07-14 20:22:56 +0800

    Update insertion_sort.h

--- I Foundations/2 Getting Started/insertion_sort/insertion_sort.h
+++ I Foundations/2 Getting Started/insertion_sort/insertion_sort.h
@@ -1,7 +1,11 @@
 #if !defined(INSERTION_SORT_H)
 #define INSERTION_SORT_H
 
-// Insertion Sort A[0 : n - 1]
+/**
+ * Insertion sort an array
+ * @param array pointer to array
+ * @param n length of array
+ */ 
 int insertion_sort(int *array, int n);
 
 #endif // INSERTION_SORT_H
